8b9c8ac8ab186bf34b82009dd959481ca6e1488c
Administrator <byz258456@163.com>
2019-01-19 00:01:34 +0800

    测试

--- src/main/java/com/waw/hr/web/TbEnterpriseController.java
+++ src/main/java/com/waw/hr/web/TbEnterpriseController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@RestController
+
 @RequestMapping("/enterprise")
 public class TbEnterpriseController {
 

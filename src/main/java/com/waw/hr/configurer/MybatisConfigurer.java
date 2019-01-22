4110a8bd70153fa50de792e686a2880c0dfa70dd
Cps <1402333029@qq.com>
2019-01-22 15:00:45 +0800

    MyBatisConfigurer驼峰命名法

--- src/main/java/com/waw/hr/configurer/MybatisConfigurer.java
+++ src/main/java/com/waw/hr/configurer/MybatisConfigurer.java
@@ -34,13 +34,14 @@ public class MybatisConfigurer {
         properties.setProperty("reasonable", "true");//页码<=0 查询第一页，页码>=总页数查询最后一页
         properties.setProperty("supportMethodsArguments", "true");//支持通过 Mapper 接口参数来传递分页参数
         pageHelper.setProperties(properties);
-
         //添加插件
         factory.setPlugins(new Interceptor[]{pageHelper});
 
         //添加XML目录
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         factory.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
+        //****开启驼峰命名
+        factory.getObject().getConfiguration().setMapUnderscoreToCamelCase(true);
         return factory.getObject();
     }
 
